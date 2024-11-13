3e2ada5ac3c85f2980900775966e5701635844be
Source-Code-007 <utsho926@gmail.com>
2024-11-13 14:13:44 +0600

    category update3d

--- src/app/module/visa/visa.service.ts
+++ src/app/module/visa/visa.service.ts
@@ -1,4 +1,3 @@
-
 import { StatusCodes } from "http-status-codes";
 import QueryBuilder from "../../builder/QueryBuilder";
 import AppError from "../../errors/appError";
@@ -6,38 +5,47 @@ import { TVisa } from "./visa.interface";
 import Visa from "./visa.model";
 
 const createVisa = async (payload: TVisa) => {
-  const isExistVisa = await Visa.findOne({passportNumber: payload?.passportNumber});
-  if(isExistVisa){
-    throw new AppError(StatusCodes.CONFLICT ,'Visa already exists with this passport number!')
+  const isExistVisa = await Visa.findOne({
+    passportNumber: payload?.passportNumber,
+  });
+  if (isExistVisa) {
+    throw new AppError(
+      StatusCodes.CONFLICT,
+      "Visa already exists with this passport number!"
+    );
   }
   const result = await Visa.create(payload);
   return result;
 };
-const getAllVisa = async (query:Record<string,unknown>) => {
+const getAllVisa = async (query: Record<string, unknown>) => {
   const visaQuery = new QueryBuilder(Visa.find(), query)
-  .searchQuery(['name', 'eVisaId', 'passportNumber'])
-  .filterQuery()
-  .paginateQuery()
-  .sortQuery()
-  .fieldFilteringQuery()
+    .searchQuery(["name", "eVisaId", "passportNumber"])
+    .filterQuery()
+    .paginateQuery()
+    .sortQuery()
+    .fieldFilteringQuery();
 
-const visas = await visaQuery.queryModel
+  const visas = await visaQuery.queryModel;
 
   // Fetch total count of documents that match the query without pagination
-  const totalVisas = await Visa.countDocuments();
+  const totalVisas = await Visa.countDocuments(
+    visaQuery.queryModel.getFilter()
+  );
 
-return {visas, totalVisas}
+  return { visas, totalVisas };
 };
 const getSingleVisa = async (id: string) => {
-  const result = await Visa.findOne({eVisaId: id});
+  const result = await Visa.findOne({ eVisaId: id });
   return result;
 };
 const deleteVisa = async (id: string) => {
   const result = await Visa.findByIdAndDelete(id);
   return result;
 };
-const updateVisa = async (id: string,updatedVisaData:Partial<TVisa>) => {
-  const result = await Visa.findByIdAndUpdate(id, updatedVisaData, {new:true});
+const updateVisa = async (id: string, updatedVisaData: Partial<TVisa>) => {
+  const result = await Visa.findByIdAndUpdate(id, updatedVisaData, {
+    new: true,
+  });
   return result;
 };
 
@@ -46,5 +54,5 @@ export const visaServices = {
   getAllVisa,
   getSingleVisa,
   deleteVisa,
-  updateVisa
-}
+  updateVisa,
+};
